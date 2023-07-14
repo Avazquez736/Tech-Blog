e48823627cfeceef91adb07f834a339666b24d99
Alvaro Vazquez <alvarovazquez@macbook-air.lan>
2023-07-14 00:28:16 -0600

    worked on hbs and connection.js files

--- server.js
+++ server.js
@@ -11,4 +11,4 @@ const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 
+const PORT = process.env.PORT || 3001;
